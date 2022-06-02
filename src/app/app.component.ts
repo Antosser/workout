a7f7cf236e6912c2db6abef03008e5219c3eea3c
Antosser <antosser@antosser.xyz>
2022-06-02 07:45:15 -0700

    Added header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,23 +11,6 @@ interface Exercise {
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'workout-angular';
-  streak = 0;
-  exercises: Exercise[] = [];
-
-  arrayMove(arr: any[], fromIndex: number, toIndex: number) {
-    var element = arr[fromIndex];
-    arr.splice(fromIndex, 1);
-    arr.splice(toIndex, 0, element);
-  }
-
-  moveUp(index: number) {
-    this.arrayMove(this.exercises, index, index - 1);
-  }
-
-  moveDown(index: number) {
-    this.arrayMove(this.exercises, index, index + 1);
-  }
 
   constructor() {
     
